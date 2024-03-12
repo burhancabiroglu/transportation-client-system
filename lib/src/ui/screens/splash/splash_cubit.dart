b47ba026e509871c12541d0c5788694cb514b2cc
burhancabiroglu <burhancabiroglu97@gmail.com>
2024-03-12 23:24:07 +0300

    splash screen completed

--- lib/src/ui/screens/splash/splash_cubit.dart
+++ lib/src/ui/screens/splash/splash_cubit.dart
@@ -1,21 +1,13 @@
 // ignore_for_file: constant_identifier_names
-import 'dart:developer';
-
 import 'package:babiconsultancy/src/backend/repo/config_repo.dart';
 import 'package:babiconsultancy/src/backend/result/app_result.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashCubit extends Cubit<SplashState> {
   final ConfigRepo configRepo;
-  SplashCubit({
-    required this.configRepo
-  }): super(SplashState.LOADING);
-  
-  Future init() async {
+  SplashCubit({required this.configRepo}): super(SplashState.LOADING) {
     configRepo.getAppConfig()
-    .successListener((value) {
-        log(value.aboutUsLink);
-    }); 
+      .successListener((value) => emit(SplashState.DONE)); 
   }
 }
 
--- lib/src/ui/screens/splash/splash_screen.dart
+++ lib/src/ui/screens/splash/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:babiconsultancy/src/core/config/assets.dart';
 import 'package:babiconsultancy/src/core/base/core_stateless_widget.dart';
 import 'package:babiconsultancy/src/core/localization/localization_keys.dart';
 import 'package:babiconsultancy/src/core/window/window_extension.dart';
+import 'package:babiconsultancy/src/ui/routes/core_router.dart';
 import 'package:babiconsultancy/src/ui/screens/main/main_screen.dart';
 import 'package:babiconsultancy/src/ui/screens/splash/splash_cubit.dart';
 import 'package:flutter/material.dart';
@@ -13,54 +14,61 @@ class SplashScreen extends CoreStatelessWidget {
   const SplashScreen({super.key});
 
 
-  void init(BuildContext context) async {
-      await BlocProvider.of<SplashCubit>(context).init();
-      Future.delayed(const Duration(milliseconds: 4000),() async {
-        await Navigator.of(context).popAndPushNamed(MainScreen.route);
-      });
-  }
-
   @override
   Widget build(BuildContext context) {
-    init(context);
-    return Scaffold(
-      appBar: AppBar(),
-      backgroundColor: theme.colorScheme.white,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Center(
-              heightFactor: 2.4,
-              child: ClipRRect(
-              borderRadius: BorderRadius.circular(500),
-              child: Assets.app_logo.toSquareImage(size: 300.h),
-              )
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 30.h),
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(bottom: 8.h),
-                    padding: EdgeInsets.all(8.h),
-                    decoration: BoxDecoration(
-                      color: theme.colorScheme.container,
-                      borderRadius: BorderRadius.circular(14)
-                    ),
-                    child: Assets.lock.toSvg(width: 24.w),
-                  ),
-                  Text(
-                    localization.of(LocalizationKeys.Splash_Description),
-                    textAlign: TextAlign.center,
-                    style: theme.textStyle.callout02,
-                  )
-                ],
+    final bloc = BlocProvider.of<SplashCubit>(context);
+    return BlocListener(
+      bloc: bloc,
+      listener: (context, state) {
+        switch(state){
+          case SplashState.DONE:
+            CoreRouter.main.popAndPushNamed(MainScreen.route);
+            break;
+          case SplashState.NETWORK_NOT_FOUND:
+            break; 
+          case SplashState.ERROR:
+            break; 
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(),
+        backgroundColor: theme.colorScheme.white,
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Center(
+                heightFactor: 2.4,
+                child: ClipRRect(
+                borderRadius: BorderRadius.circular(500),
+                child: Assets.app_logo.toSquareImage(size: 300.h),
+                )
               ),
-            ),
-          ],
+              Container(
+                margin: EdgeInsets.only(bottom: 30.h),
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 8.h),
+                      padding: EdgeInsets.all(8.h),
+                      decoration: BoxDecoration(
+                        color: theme.colorScheme.container,
+                        borderRadius: BorderRadius.circular(14)
+                      ),
+                      child: Assets.lock.toSvg(width: 24.w),
+                    ),
+                    Text(
+                      localization.of(LocalizationKeys.Splash_Description),
+                      textAlign: TextAlign.center,
+                      style: theme.textStyle.callout02,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
